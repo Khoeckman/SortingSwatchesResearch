5d3cd25321ed7dc9ca10cbef1d3b3202f5623d36
Kyle Hoeckman <kyle.hoeckman@gmail.com>
2026-02-11 23:47:04 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/ColorSwatchesSortingResearch/',
+  base: '/SortingSwatchesResearch/',
   build: {
     outDir: 'docs',
   },
